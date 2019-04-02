77e379c17126d5aaa35390490ca8b868c4a51a3b
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-04-01 22:54:24 -0600

    Update StringInput.cpp

--- StringInput.cpp
+++ StringInput.cpp
@@ -9,9 +9,19 @@ using namespace std;
 int main ()
 {
   int i;
+  unsigned j;
   string mystr;
   cout << "Please enter an integer: ";
   getline (cin , mystr);
+  
+  cout << "The digits of your integer are: ";
+  for (char c : mystr)
+  {
+      j++;
+      cout << c;
+      j < mystr.length() ? cout << ", " : cout << " ";
+  }
+  
   stringstream (mystr) >> i;
   cout << "The double is " << i*2 << ".\n";
   return 0;
