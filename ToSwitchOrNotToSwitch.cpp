f41f3658f89b96f44779897e26889f96da2fc0da
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-04-01 23:21:04 -0600

    Create ToSwitchOrNotToSwitch.cpp

---
+++ ToSwitchOrNotToSwitch.cpp
@@ -0,0 +1,36 @@
+// switch control flow
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  string mystr;
+  cout << "Please enter a string: ";
+  getline (cin , mystr);
+  
+  char c = mystr[0];
+  switch (c) 
+  {
+      case 'a':
+      cout << "The first letter is the vowel: "<< 'a';
+      break;
+      case 'e':
+      cout << "The first letter is the vowel: "<< 'e';
+      break;
+      case 'i':
+      cout << "The first letter is the vowel: "<< 'i';
+      break;
+      case 'o':
+      cout << "The first letter is the vowel: "<< 'o';
+      break;
+      case 'u':
+      cout << "The first letter is the vowel: "<< 'u';
+      break;
+      default: 
+      cout << "The first letter is not a vowel";
+  }
+  return 0;
+}
