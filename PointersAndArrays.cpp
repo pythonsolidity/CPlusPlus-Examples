83ed5dfb45e8244ba34ef676a05b3dafb0933a38
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-04-14 00:29:34 -0600

    Create PointersAndArrays.cpp

---
+++ PointersAndArrays.cpp
@@ -0,0 +1,37 @@
+// pointers and arrays
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int arr[6];
+    int * pt;
+  
+    pt = arr;     // no & operator, an array points to its first element 
+    *pt = 1;
+  
+    pt++;  
+    *pt = 2;
+  
+    pt = arr + 2;  
+    *pt = 3;
+  
+    pt = &arr[3];  // need to use the & operator here
+    *pt = 4;
+  
+    pt = arr;     // pt again points to the first arr element 
+    *(pt+4) = 5;
+  
+    pt = (arr+5);
+    *pt = 6;
+
+    cout << "The address of the first element of arr = "<< arr << "\n";
+    cout << "The first element of arr = "<< *arr << "\n";
+
+    cout << "The elements of array arr: ";
+    for (int a : arr){                              // range-based for loop
+        cout << a << ", ";
+    }
+    
+    return 0;
+}
