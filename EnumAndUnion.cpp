26127da474ecd3e279e6c93afa58cdb383904f7e
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-04-14 18:48:33 -0600

    Update EnumAndUnion.cpp

--- EnumAndUnion.cpp
+++ EnumAndUnion.cpp
@@ -1,14 +1,21 @@
-// enum and union
+// enum, enum class and union
 #include <iostream>
 using namespace std;
 
 int main ()
 {
   enum math {calculus, algebra = 3, geometry} course;   
-  
   course = math(4);                             /* calculus is implicitly converted to the integer 0, 
                                                 algebra is explicitly converted to 3 and geometry to 4 */
-  cout << (course == geometry) <<'\n' ;
+  cout << (course == geometry) <<'\n';
+  course = calculus; 
+  cout << course << '\n';
+  
+  enum class Math {calculus, algebra, geometry} subject;
+  subject = Math::calculus;                     // subject = calculus; throws an error
+  subject = Math(1);
+  cout << (subject == Math::algebra) <<'\n';   /* no implicit conversion to integers, 
+                                                    so cout << subject; throws an error */
   
   union Same {
     char ch[4] = {'B', 'a', 'y', '\0'};
