1d4ee09b7aed6a359ab9c89d6d729388e7ca58f6
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-04-14 18:35:47 -0600

    Update and rename UnionEnum.cpp to EnumAndUnion.cpp

--- EnumAndUnion.cpp
+++ EnumAndUnion.cpp
@@ -1,22 +1,21 @@
-// unions and enums
+// enum and union
 #include <iostream>
 using namespace std;
 
 int main ()
 {
+  enum math {calculus, algebra = 3, geometry} course;   
+  
+  course = math(4);                             /* calculus is implicitly converted to the integer 0, 
+                                                algebra is explicitly converted to 3 and geometry to 4 */
+  cout << (course == geometry) <<'\n' ;
+  
   union Same {
     char ch[4] = {'B', 'a', 'y', '\0'};
     int n;
   } un;
   
-  cout << un.ch[2] << ", " << un.n <<'\n';      // Both un.ch and un.n refer to the same value
-    
-  enum math {calculus, algebra = 3, geometry} course;   
-  
-  course = math(4);                             /* calculus is implicitly converted to the integer 0, 
-                                                algebra is explicitly converted to 3 and geometry to 4 */
-  
-  cout << (course == geometry) ;
+  cout << un.ch[2] << ", " << un.n;      // Both un.ch and un.n refer to the same value
   
   return 0;
 }
